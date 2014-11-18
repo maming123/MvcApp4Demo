be7e2d3de2151c3d334a270ac053ad7f5300a01a
maming123 <hurricanefbi@163.com>
2014-11-18 17:06:15 +0800

    同步

---
+++ MvcApp4Demo/Module/Utils/NinjectControllerFactory .cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using MvcApp4Demo.Module.Email;
+using Ninject;
+namespace MvcApp4Demo.Module.Utils
+{
+    public class NinjectControllerFactory:DefaultControllerFactory
+    {
+        //  Ninject中用来管理接口和实例的核心Ninject接口
+        private IKernel ninjectKernel;
+        public NinjectControllerFactory()
+        {
+            // 通过new StandardKernel得到
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        //  通过重写此方法实现用Ninject提供接口的实例
+        protected override IController GetControllerInstance(RequestContext requestContext,
+        Type controllerType)
+        {
+            return controllerType == null
+            ? null
+            : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        //  实现接口和实例类绑定的代码均在此方法中
+        private void AddBindings()
+        {
+            //因为ninject不支持从配置文件配置 也可以通过ninject扩展xml从文件读取配置
+
+            //可以通过此处绑定，更改接口不同的实现方法，用来完成后台不同处理的实现
+             ninjectKernel.Bind<IEmailHelper>().To<EmailHelper>();
+
+            //实现当MvcApp4Demo.Controllers.HomeController类中需要该接口的实例时，会把EmailHelper163绑定到IEmailHelper接口，解决统一接口的不同类实现到不同的接口的问题
+            //ninjectKernel.Bind<IEmailHelper>().To<EmailHelper163>().WhenInjectedInto< MvcApp4Demo.Controllers.HomeController>();
+        }
+    }
+}
---
+++ MvcApp4Demo/Module/WeiXin/ReceiveMsgManager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+namespace MvcApp4Demo.Module.WeiXin
+{
+    public class ReceiveMsgManager
+    {
+        private XmlNode _XmlNode ;
+        private ReceiveMsgItem _MsgItem;
+        public ReceiveMsgManager(string xmlBody)
+        {
+            XmlDocument xmlDoc = new System.Xml.XmlDocument();
+            xmlDoc.LoadXml(xmlBody);
+            _XmlNode = xmlDoc.DocumentElement;
+
+            _MsgItem = GetMsgItem();
+        }
+
+        public ReceiveMsgItem GetMsgItem()
+        {
+            ReceiveMsgItem msgItem = new ReceiveMsgItem();
+            string msgType = _XmlNode.SelectSingleNode("MsgType").InnerText;
+            msgItem.MsgType=(MsgType)Enum.Parse(typeof(MsgType), msgType);
+            msgItem.ToUserName = _XmlNode.SelectSingleNode("ToUserName").InnerText;
+            msgItem.FromUserName = _XmlNode.SelectSingleNode("FromUserName").InnerText;
+            msgItem.MsgId = long.Parse(_XmlNode.SelectSingleNode("MsgId").InnerText);
+            msgItem.CreateTime = int.Parse(_XmlNode.SelectSingleNode("CreateTime").InnerText);
+            return msgItem;
+        }
+
+        /// <summary>
+        /// 解析接收的文本信息
+        /// </summary>
+        /// <returns></returns>
+        public ReceiveTextMsgItem GetTextMsgItem()
+        {
+            ReceiveTextMsgItem txtMsg = new ReceiveTextMsgItem() { FromUserName=_MsgItem.FromUserName, ToUserName=_MsgItem.ToUserName
+            , CreateTime=_MsgItem.CreateTime
+            , MsgId=_MsgItem.MsgId
+            , MsgType=_MsgItem.MsgType};
+
+            txtMsg.Content = _XmlNode.SelectSingleNode("Content").InnerText;
+            return txtMsg;
+        }
+
+    }
+    public class ReceiveMsgItem
+    {
+        //<xml><ToUserName><![CDATA[gh_62b877ddf2dc]]></ToUserName>
+//<FromUserName><![CDATA[oNK_qjgPljFVprXzWC7hnJmxyL18]]></FromUserName>
+//<CreateTime>1416283659</CreateTime>
+//<MsgType><![CDATA[text]]></MsgType>
+//<Content><![CDATA[测试]]></Content>
+//<MsgId>6082891997465315618</MsgId>
+//</xml>
+
+
+       private MsgType _MsgType = MsgType.text;
+
+        public MsgType MsgType
+        {
+            get { return _MsgType; }
+            set { _MsgType = value; }
+        }
+
+        private long _MsgId = 0;
+
+        public long MsgId
+        {
+            get { return _MsgId; }
+            set { _MsgId = value; }
+        }
+
+        private int _CreateTime = 0;
+
+        public int CreateTime
+        {
+            get { return _CreateTime; }
+            set { _CreateTime = value; }
+        }
+        private string _FromUserName = "";
+
+        public string FromUserName
+        {
+            get { return _FromUserName; }
+            set { _FromUserName = value; }
+        }
+        private string _ToUserName = "";
+
+        public string ToUserName
+        {
+            get { return _ToUserName; }
+            set { _ToUserName = value; }
+        }
+    }
+
+    public enum MsgType:int
+    {
+        text
+    }
+
+    public class ReceiveTextMsgItem : ReceiveMsgItem
+    {
+        private string _Content = "";
+
+        public string Content
+        {
+            get { return _Content; }
+            set { _Content = value; }
+        }
+    }
+}
